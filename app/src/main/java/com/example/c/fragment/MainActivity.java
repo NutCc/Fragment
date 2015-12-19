14f4f94be2cfddc2735f4be123087869643d8b48
nut c <951284984@qq.com>
2015-12-19 22:29:35 +0800

    终于可以三个界面了，师父不要打我TAT

--- app/src/main/java/com/example/c/fragment/MainActivity.java
+++ app/src/main/java/com/example/c/fragment/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.c.fragment;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.DialogInterface;
@@ -16,10 +17,19 @@ public class MainActivity extends Activity implements View.OnClickListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //很不懂为什么要new两次
+        AnotherRightFragment fragment=new AnotherRightFragment();
+        FragmentManager fragmentManager=getFragmentManager();
+        FragmentTransaction transaction=fragmentManager.beginTransaction();
+        transaction.replace(R.id.right_layout, fragment);
+        transaction.commit();
+        //
         Button button= (Button) findViewById(R.id.button);
         button.setOnClickListener(this);
-       // Button button3= (Button) findViewById(R.id.button3);//这里有改动
-        //button3.setOnClickListener(this);
+        Button button2= (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(this);
+        Button button3= (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(this);
     }
 
     @Override
@@ -30,16 +40,21 @@ public class MainActivity extends Activity implements View.OnClickListener {
                 FragmentManager fragmentManager=getFragmentManager();
                 FragmentTransaction transaction=fragmentManager.beginTransaction();
                 transaction.replace(R.id.right_layout, fragment);
-                transaction.addToBackStack(null);
                 transaction.commit();
                 break;
-            /*case R.id.button3:      //这里是点击第三个按钮的情况
-                RightFragment fragment1=new RightFragment();
-                FragmentManager fragmentManager1=getFragmentManager();
-                FragmentTransaction transaction1=fragmentManager1.beginTransaction();
-                transaction1.replace(R.id.fragment_right, fragment1);
-                transaction1.commit();
-                break;*/
+            case R.id.button2:
+               ZLeftFragment fragment2=new ZLeftFragment();
+                FragmentManager fragmentManager2=getFragmentManager();
+                FragmentTransaction transaction2=fragmentManager2.beginTransaction();
+                transaction2.replace(R.id.right_layout, fragment2);
+                transaction2.commit();
+                break;
+            case R.id.button3:
+                ZRightFragment fragment3=new ZRightFragment();
+                FragmentManager fragmentManager3=getFragmentManager();
+                FragmentTransaction transaction3=fragmentManager3.beginTransaction();
+                transaction3.replace(R.id.right_layout,fragment3);
+                transaction3.commit();
         }
     }
 }
---
+++ app/src/main/java/com/example/c/fragment/ZRightFragment.java
@@ -0,0 +1,20 @@
+package com.example.c.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by c on 2015/12/19.
+ */
+public class ZRightFragment extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.fragment_zright,container,false);
+        return view;
+    }
+}
